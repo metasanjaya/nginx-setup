743b82029a47ce262c060944fe5dba0a6445baf4
Meta Sanjaya <metasanjaya@gmail.com>
2025-02-12 15:19:50 +0700

    add nginx init script and cloudflare firewall whitelist

--- setup.sh
+++ setup.sh
@@ -6,9 +6,14 @@ add-apt-repository ppa:ondrej/nginx -y
 apt install nginx-full -y
 mkdir -p /tmp/nginx
 mkdir -p /var/www/html
-chown -R www-data:www-data /tmp/nginx/
+chown -R www-data:www-data /tmp/nginx
 cp -rf /etc/nginx /etc/nginx.bak
+rm -rf /etc/nginx
 cp -rf ./nginx /etc/nginx
+mkdir -p /root/init.d
+cp -rf /etc/init.d/nginx /root/init.d/nginx
+rm -f /etc/init.d/nginx
+cp -rf ./init.d/nginx /etc/init.d/nginx
 openssl req -x509 -newkey rsa:4096 -keyout /etc/nginx/ssl/self-signed-key.pem -out /etc/nginx/ssl/self-signed-cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=LO/L=LOCAL/O=LOCALNET/OU=LOCAL/CN=localhost"
 openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
 
@@ -17,3 +22,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install 20
+
+## UFW SETTING 
+# ufw allow 22
+# ufw enable
+# for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done
+# for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto udp from $cfip to any port 443 comment 'Cloudflare IP'; done 
+# ufw reload
